b6a117b685aa7bad3ffef8a92c867c9acd4d9137
Yash <yash@wabridge.in>
2026-01-08 19:33:25 +0530

    Update wabasignup to use Vercel API endpoint

--- wabasignup/CTWhatsAppBusinessFB.js
+++ wabasignup/CTWhatsAppBusinessFB.js
@@ -1,9 +1,9 @@
-jQuery.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+jQuery.getScript('https://connect.facebook.net/en_US/sdk.js', function () {
     FB.init({
-        appId:    '1087237666105030', // Facebook App ID
-        cookie:   true, // enable cookies
-        xfbml:    true, // parse social plugins on this page
-        version:  'v20.0' //Graph API version
+        appId: '2704195743293039', // Facebook App ID
+        cookie: true, // enable cookies
+        xfbml: true, // parse social plugins on this page
+        version: 'v20.0' //Graph API version
     });
 });
 
@@ -13,37 +13,53 @@ jQuery('#connect_now').on('click', function (e) {
         console.log(response);
         if (response.authResponse) {
             const code = response.authResponse.code;
-            const appId = '1087237666105030';
-            const appSecretId = '6a4cd52ef4b49c489b92642cc54de9b8';
+            const appId = '2704195743293039';
+            const appSecretId = '494585bd885e60111e50ce31dcf2ba7a';
 
             var params = {
-                'client_id' : appId, //app_id
-                'client_secret' : appSecretId, //App secret
-                'code' : code            }
+                'client_id': appId, //app_id
+                'client_secret': appSecretId, //App secret
+                'code': code
+            }
 
             $.ajax({
                 type: "POST",
-                url: 'getDataAfterConnectFB.php',
-                data: params,
+                url: '/api/exchange-token',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    code: code
+                }),
                 dataType: 'json',
-                success: function(response){
-                    //console.log(response);
+                success: function (response) {
+                    console.log('Token exchange response:', response);
                     var accessToken = response.access_token;
                     var whatsappBusinessAccountId = response.whatsapp_business_account_id;
-                    var phoneNumberId = response.phone_number_id;
-                    var formattedPhoneNumber = response.formatted_phone_number;
+
+                    // Handle phone numbers array
+                    var phoneNumberId = null;
+                    var formattedPhoneNumber = null;
+                    if (response.phone_numbers && response.phone_numbers.length > 0) {
+                        phoneNumberId = response.phone_numbers[0].id;
+                        formattedPhoneNumber = response.phone_numbers[0].formatted_phone_number || response.phone_numbers[0].display_phone_number;
+                    }
 
                     sendDataToParent({
                         appId: appId,
                         accessToken: accessToken,
                         whatsappBusinessAccountId: whatsappBusinessAccountId,
                         phoneNumberId: phoneNumberId,
-                        formattedPhoneNumber: formattedPhoneNumber
+                        formattedPhoneNumber: formattedPhoneNumber,
+                        phoneNumbers: response.phone_numbers || []
                     });
                     hideLoader();
                     window.close();
-                }//end of success
-            });               
+                },
+                error: function (xhr, status, error) {
+                    console.error('Token exchange error:', xhr.responseText);
+                    hideLoader();
+                    alert('Connection failed: ' + (xhr.responseJSON?.error || error));
+                }
+            });
         } else {
             hideLoader();
             if (response.error) {
@@ -52,26 +68,26 @@ jQuery('#connect_now').on('click', function (e) {
                 alert('Login failed');
             }
         }
-    }, 
-    {
-        // config_id: '1264392078061570',
-        // response_type: 'code',
-        // override_default_response_type: true,
-        // scope: 'public_profile, business_management, whatsapp_business_management, whatsapp_business_messaging, catalog_management',
-        // extras: {
-        //     "feature": "whatsapp_embedded_signup",
-        //     "sessionInfoVersion": 3  //  Receive Session Logging Info
-        // }
-        	config_id: "1264392078061570",
-		response_type: "code",
-		override_default_response_type: true,
-		scope: "public_profile, business_management, whatsapp_business_management, whatsapp_business_messaging, catalog_management",
-		extras: {
-		  setup: {},
-		  featureType: "whatsapp_business_app_onboarding",
-		  sessionInfoVersion: "3"
-		}
-    });
+    },
+        {
+            // config_id: '1264392078061570',
+            // response_type: 'code',
+            // override_default_response_type: true,
+            // scope: 'public_profile, business_management, whatsapp_business_management, whatsapp_business_messaging, catalog_management',
+            // extras: {
+            //     "feature": "whatsapp_embedded_signup",
+            //     "sessionInfoVersion": 3  //  Receive Session Logging Info
+            // }
+            config_id: "879357534817865",
+            response_type: "code",
+            override_default_response_type: true,
+            scope: "public_profile, business_management, whatsapp_business_management, whatsapp_business_messaging, catalog_management",
+            extras: {
+                setup: {},
+                featureType: "whatsapp_business_app_onboarding",
+                sessionInfoVersion: "3"
+            }
+        });
 });
 // config_id: "1264392078061570",
 // response_type: "code",
